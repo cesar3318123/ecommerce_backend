0915b15ec4db2acba1755ddc615d47d344ab7d74
cesar3318123 <www.cesar3318123@gmail.com>
2025-09-05 23:05:38 -0600

    mis nuevos cambios

--- models/CartItem.js
+++ models/CartItem.js
@@ -2,30 +2,38 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/database"); // tu conexión
 
-const CartItem = sequelize.define("CartItem", {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const CartItem = sequelize.define(
+  "CartItem",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    productId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    marca: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    imagen: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
   },
-  userId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  nombre: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  marca: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  imagen: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-}, {
-  timestamps: true,
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = CartItem;
