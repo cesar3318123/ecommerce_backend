b56a663e40e7b8d81cb0ee11724e6083825eaceb
cesar3318123 <www.cesar3318123@gmail.com>
2025-09-12 10:21:05 -0600

    mis nuevos cambios

--- models/prueba4.js
+++ models/prueba4.js
@@ -1,18 +1,26 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database'); // Ajusta la ruta a tu instancia de Sequelize
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database"); // Ajusta la ruta a tu instancia de Sequelize
 
-const Prueba4 = sequelize.define('Prueba4', {
-  preciso: { type: DataTypes.BOOLEAN, allowNull: false },
-  preferencia: { type: DataTypes.BOOLEAN, allowNull: false },
-  entiendeConsulta: { type: DataTypes.BOOLEAN, allowNull: false },
-  innovador: { type: DataTypes.BOOLEAN, allowNull: false },
-  lenguajeNatural: { type: DataTypes.BOOLEAN, allowNull: false },
-  facilidad: { type: DataTypes.BOOLEAN, allowNull: false },
-  moderno: { type: DataTypes.BOOLEAN, allowNull: false },
-  pocoTiempo: { type: DataTypes.BOOLEAN, allowNull: false },
-}, {
-  tableName: 'Prueba4',
-  timestamps: true
-});
+const Prueba4 = sequelize.define(
+  "Prueba4",
+  {
+    preciso: { type: DataTypes.BOOLEAN, allowNull: false },
+    preferencia: { type: DataTypes.BOOLEAN, allowNull: false },
+    entiendeConsulta: { type: DataTypes.BOOLEAN, allowNull: false },
+    innovador: { type: DataTypes.BOOLEAN, allowNull: false },
+    lenguajeNatural: { type: DataTypes.BOOLEAN, allowNull: false },
+    facilidad: { type: DataTypes.BOOLEAN, allowNull: false },
+    moderno: { type: DataTypes.BOOLEAN, allowNull: false },
+    pocoTiempo: { type: DataTypes.BOOLEAN, allowNull: false },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "Prueba4",
+    timestamps: true,
+  }
+);
 
 module.exports = Prueba4;
